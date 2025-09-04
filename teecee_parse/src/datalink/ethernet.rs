1f8f700b5d86b420b7adcd591c5923dc88bec7a7
Nahla Hashim <nahla.nee@protonmail.com>
2025-09-04 12:40:25 -0600

    remove commented out consts

--- teecee_parse/src/datalink/ethernet.rs
+++ teecee_parse/src/datalink/ethernet.rs
@@ -2,24 +2,12 @@ use std::fmt::{Debug, Display};
 
 use zerocopy::{big_endian, FromBytes, Immutable, IntoBytes, KnownLayout};
 
-use crate::datalink::arp::ArpHeader;
-
 #[repr(transparent)]
 #[derive(FromBytes, IntoBytes, KnownLayout, Immutable)]
 pub struct MacAddr([u8; 6]);
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Ethertype(big_endian::U16);
-
-// impl Ethertype {
-//     pub const IPV4: Self = Self(0x0800);
-//     pub const ARP: Self = Self(0x0806);
-//     pub const ETHERNET: Self = Self(0x6558);
-//     pub const VLAN: Self = Self(0x8100);
-//     pub const IPV6: Self = Self(0x86dd);
-//     pub const LLDP: Self = Self(0x88cc);
-//     pub const QINQ: Self = Self(0x9100);
-// }
 
 #[repr(C)]
 #[derive(FromBytes, KnownLayout, Immutable, Debug)]
